baccf8257d7a71444b1c48da39e59ea8ccac4440
李继龙 <kljzndx@outlook.com>
2024-08-05 12:14:10 +0800

    新增分类面板组件

---
+++ src/components/CategoryPanel.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import ArticleGroup from './ArticleGroup.vue'
+
+import { data as Dmicro } from '../micro/micro.data.ts'
+import { data as Dtech } from '../tech/tech.data.ts'
+import { data as Dlife } from '../about/life/life.data.ts'
+
+const data = [
+    {
+        title: "微博",
+        url: "micro/",
+        items: Dmicro,
+    },
+    {
+        title: "技术",
+        url: "tech/",
+        items: Dtech,
+    },
+    {
+        title: "自传",
+        url: "about/life/",
+        items: Dlife,
+    },
+]
+
+</script>
+
+<template>
+    <ul :class="$style['cate-panel']">
+        <li :class="$style['cate-item']" v-for="e of data">
+            <ArticleGroup :items="e.items" :title="e.title" :url="e.url" />
+        </li>
+    </ul>
+</template>
+
+<style module>
+ul.cate-panel {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 2rem;
+    margin: 0;
+    padding: 0;
+}
+
+li.cate-item {
+    list-style: none;
+}
+
+li.cate-item+li {
+    margin: 0;
+}
+</style>
